aa72d8a341ce91c4efd42e04c00a1559f3482401
rajkadam16 <152425457+rajkadam16@users.noreply.github.com>
2024-05-12 19:19:19 +0530

    added styling

--- From/src/app/components/register/register.component.ts
+++ From/src/app/components/register/register.component.ts
@@ -11,12 +11,12 @@ export class RegisterComponent {
 submitDetails() {
 throw new Error('Method not implemented.');
 }
-
   registerFrom =this.fb.group({
     fullName:['',[Validators.required,Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)]],
     email:['',[Validators.required,Validators.email]],
     password:['',Validators.required],
-    confirmPassword:['',Validators.required]
+    confirmPassword:['',Validators.required],
+    phonenumber: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]]  
 
   })
   constructor(private fb: FormBuilder){
@@ -26,6 +26,9 @@ throw new Error('Method not implemented.');
   }
   get email(){
     return this.registerFrom.controls['email'];
+  }
+  get Phonenumber(){
+    return this.registerFrom.controls['phonenumber'];
   }
   get password(){
     return this.registerFrom.controls['password'];
