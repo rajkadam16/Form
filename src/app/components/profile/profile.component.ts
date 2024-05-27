6a607938232a25ce7a12c4ca6cf3bd002eb096bc
rajkadam16 <152425457+rajkadam16@users.noreply.github.com>
2024-05-27 22:45:03 +0530

    fix(dev): add Dynamic in profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JsonfilereaderService } from 'src/app/services/jsonfilereader.service';
+import { SystemConfig } from 'src/app/shared/systemconfig';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  profileData: any[] = [];
+
+  constructor(public faq: JsonfilereaderService) { }
+
+  ngOnInit(): void {
+    this.faq.parseJsonFile(SystemConfig.profileJson).subscribe((res: any) => {
+      this.profileData = res;
+    });
+  }
+  
+
 
 }
--- src/app/shared/systemconfig.ts
+++ src/app/shared/systemconfig.ts
@@ -1,4 +1,7 @@
 export const SystemConfig = {
     notificationJson: "assets/master/sys-notificaton.json",
     navigatorJson: "assets/master/navbar.json",
+    faqJson: "assets/master/faq.json",
+    profileJson: "assets/master/profile.json",
+
 }
