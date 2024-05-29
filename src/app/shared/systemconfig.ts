a2c68cb35b3f7508fee68540ef6cbc3a34c0a1d3
rajkadam16 <152425457+rajkadam16@users.noreply.github.com>
2024-05-29 21:21:53 +0530

    fix(dev): add footer Dynamiclly

--- src/app/shared/systemconfig.ts
+++ src/app/shared/systemconfig.ts
@@ -4,5 +4,6 @@ export const SystemConfig = {
     faqJson: "assets/master/faq.json",
     profileJson: "assets/master/profile.json",
     logoutJson: "assets/master/logout.json",
+    footerJson: "assets/master/footer.json",
 
 }
