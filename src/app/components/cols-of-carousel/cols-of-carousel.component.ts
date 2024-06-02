718614dfc377ae88de2a3f7dd06a61d63ee6cbff
Ramdas Kadam <ramdaskadam90@gmail.com>
2024-06-01 19:24:15 -0500

    fix(dev): added cols of carousel comp

---
+++ src/app/components/cols-of-carousel/cols-of-carousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cols-of-carousel',
+  templateUrl: './cols-of-carousel.component.html',
+  styleUrls: ['./cols-of-carousel.component.css']
+})
+export class ColsOfCarouselComponent {
+
+}
